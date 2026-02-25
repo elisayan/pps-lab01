18ceee8195caabf5c7a092e76bd01d3262cec3e9
Elisa Yan <elisa.yan@studio.unibo.it>
2026-02-25 23:18:53 +0100

    implemented SmartDoorLock

---
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
@@ -0,0 +1,74 @@
+package tdd;
+
+public class SmartDoorLockImpl implements SmartDoorLock {
+
+    public static final int MAX_ATTEMPTS = 3;
+    private static final int PIN_LENGTH = 4;
+
+    private boolean locked = false;
+    private Integer pin = null;
+    private int failedAttempts = 0;
+
+    @Override
+    public void setPin(int pin) {
+        if (isLocked()) {
+            throw new IllegalStateException("Cannot set PIN while locked");
+        }
+        if (isBlocked()) {
+            throw new IllegalStateException("Cannot set PIN while blocked");
+        }
+        if (String.valueOf(pin).length() != PIN_LENGTH) {
+            throw new IllegalArgumentException("PIN must be " + PIN_LENGTH + " digits");
+        }
+        this.pin = pin;
+    }
+
+    @Override
+    public void unlock(int pin) {
+        if (this.pin == null) {
+            throw new IllegalStateException("PIN not set");
+        }
+
+        if (pin == this.pin && !this.isBlocked()) {
+            this.locked = false;
+            this.failedAttempts = 0;
+        } else {
+            this.failedAttempts++;
+        }
+    }
+
+    @Override
+    public void lock() {
+        if (this.pin == null) {
+            throw new IllegalStateException("Cannot lock: PIN not set");
+        }
+        this.locked = true;
+    }
+
+    @Override
+    public boolean isLocked() {
+        return this.locked;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return this.failedAttempts >= MAX_ATTEMPTS;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return MAX_ATTEMPTS;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return this.failedAttempts;
+    }
+
+    @Override
+    public void reset() {
+        this.pin = null;
+        this.locked = false;
+        this.failedAttempts = 0;
+    }
+}
