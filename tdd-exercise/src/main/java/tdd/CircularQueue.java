f6ba9185994972140981132deedf46eae6c60cd8
Elisa Yan <elisa.yan@studio.unibo.it>
2026-02-26 20:44:58 +0100

    added javadoc for methods

--- tdd-exercise/src/main/java/tdd/CircularQueue.java
+++ tdd-exercise/src/main/java/tdd/CircularQueue.java
@@ -14,17 +14,56 @@ package tdd;
  *   - Refactor
  */
 public interface CircularQueue {
+
+    /**
+     * Adds a value to the queue
+     * If the queue is full, the oldest element is overwritten
+     *
+     * @param value the value to add
+     */
     void enqueue(int value);
 
+    /**
+     * Removes and returns the oldest element in the queue
+     *
+     * @return the removed element
+     * @throws IllegalStateException if the queue is empty
+     */
     int dequeue();
 
+    /**
+     * Removes and returns the oldest element in the queue
+     *
+     * @return the removed element
+     * @throws IllegalStateException if the queue is empty
+     */
     int peek();
 
+    /**
+     * Checks whether the queue contains no elements
+     *
+     * @return true if empty, false otherwise
+     */
     boolean isEmpty();
 
+    /**
+     * Checks whether the queue has reached its capacity
+     *
+     * @return true if full, false otherwise
+     */
     boolean isFull();
 
+    /**
+     * Returns the number of elements currently stored
+     *
+     * @return the current size
+     */
     int size();
 
+    /**
+     * Returns the fixed maximum capacity of the queue
+     *
+     * @return the capacity
+     */
     int capacity();
 }
